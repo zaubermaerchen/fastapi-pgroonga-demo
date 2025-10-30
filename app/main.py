b1d30220b255bb228a27834945c330b32a5ec21b
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-10-30 16:10:19 +0900

    main.pyからルーティング設定分離

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 
+from app.api.router import router
 from app.core.config import get_settings
 
 settings = get_settings()
 
 app = FastAPI(debug=settings.DEBUG)
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(router)
