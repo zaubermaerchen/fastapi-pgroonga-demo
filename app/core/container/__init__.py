68dec0e9f543c3d310236bb1040cdf0163e31ea5
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-10-31 14:49:14 +0900

    DBのセッションをDIコンテナからとれるようにする

--- app/core/container/__init__.py
+++ app/core/container/__init__.py
@@ -3,6 +3,8 @@ from dishka.integrations.fastapi import FastapiProvider
 
 from app.core.config import Settings
 
+from .provider.database import DatabaseProvider
 
-def make_container(_: Settings):
-    return make_async_container(FastapiProvider())
+
+def make_container(settings: Settings):
+    return make_async_container(DatabaseProvider(settings), FastapiProvider())
---
+++ app/core/container/provider/database.py
@@ -0,0 +1,44 @@
+from collections.abc import AsyncGenerator
+
+from dishka import Provider, Scope, provide
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.pool import NullPool
+
+from app.core.config import Settings
+
+
+class DatabaseProvider(Provider):
+    def __init__(self, settings: Settings):
+        super().__init__()
+        self.settings = settings
+
+    @provide(scope=Scope.APP)
+    def engine(self) -> AsyncEngine:
+        return create_async_engine(
+            self.settings.DATABASE_URL,
+            echo=self.settings.DEBUG,
+            future=True,
+            pool_pre_ping=True,
+            poolclass=NullPool,
+        )
+
+    @provide(scope=Scope.APP)
+    def session_pool(self, engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
+        return async_sessionmaker(
+            bind=engine,
+            class_=AsyncSession,
+            autoflush=False,
+            autocommit=False,
+        )
+
+    @provide(scope=Scope.REQUEST)
+    async def session(
+        self, session_pool: async_sessionmaker[AsyncSession]
+    ) -> AsyncGenerator[AsyncSession]:
+        async with session_pool() as session:
+            yield session
