2fb557339770e39240976f89af5bdbbe3ab212b4
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-10-31 19:40:35 +0900

    コンテナ生成部をmain.pyに移動

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
-from dishka.integrations.fastapi import setup_dishka
+from dishka import make_async_container
+from dishka.integrations.fastapi import FastapiProvider, setup_dishka
 from fastapi import FastAPI
 
 from app.api.router import router
 from app.core.config import get_settings
-from app.core.container import make_container
+from app.core.container.provider import make_providers
 
 settings = get_settings()
 
 app = FastAPI(debug=settings.DEBUG)
 
-setup_dishka(make_container(settings), app)
+container = make_async_container(*make_providers(settings), FastapiProvider())
+setup_dishka(container, app)
 
 app.include_router(router)
