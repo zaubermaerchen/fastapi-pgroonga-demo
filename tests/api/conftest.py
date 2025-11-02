d889b0c02fb09a61826e63a79ef7615df8c6abab
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2025-11-02 15:50:40 +0900

    AsyncClient生成部分をfixture化

---
+++ tests/api/conftest.py
@@ -0,0 +1,12 @@
+import pytest_asyncio
+from httpx import ASGITransport, AsyncClient
+
+from app.main import app
+
+
+@pytest_asyncio.fixture
+async def client():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        yield ac
--- tests/api/test_router.py
+++ tests/api/test_router.py
@@ -1,14 +1,9 @@
 import pytest
-from httpx import ASGITransport, AsyncClient
-
-from app.main import app
+from httpx import AsyncClient
 
 
 @pytest.mark.anyio
-async def test_root():
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        response = await ac.get("/")
+async def test_root(client: AsyncClient):
+    response = await client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
